55dbae684cef595f7d2296d68f8fbd7118f774ba
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-06-29 10:02:34 +0300

    fix: add  environment variables for production build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: '',
-  supabase_api_key:''
+  baseUrl: 'https://vpugcrolnalqsehhpein.supabase.co',
+  supabase_api_key: 'sb_publishable_Unq_YOOIQmkIqI8yOyEZ7A_OGst2f9p'
 };
