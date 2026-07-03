7b54fb82633b0ea46ecf0c770b1ed59d767ec0bd
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-03 04:27:10 +0300

    fix: resolve RouterStateSnapshot injection error

--- src/app/core/guards/reverse-auth.guard.ts
+++ src/app/core/guards/reverse-auth.guard.ts
@@ -2,11 +2,10 @@ import { CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthServiceService } from '../../features/auth/services/auth-service.service';
 import { inject } from '@angular/core';
 
-export const reverseAuthGuard: CanActivateFn = () => {
+export const reverseAuthGuard: CanActivateFn = (_route,state: RouterStateSnapshot) => {
   const _auth = inject(AuthServiceService);
   const _router = inject(Router);
-  const _state = inject(RouterStateSnapshot)
-  const current_url = _state.url;
+  const current_url = state.url;
 
   if (current_url.includes('access_token') || current_url.includes('otp_expired')) {
     return true; 
