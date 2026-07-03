0cab81f307773abe6abddb9f5285e8cea58f98b6
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-03 04:17:21 +0300

    fix: allow password recovery links in reverseAuthGuard

--- src/app/core/guards/reverse-auth.guard.ts
+++ src/app/core/guards/reverse-auth.guard.ts
@@ -1,10 +1,16 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthServiceService } from '../../features/auth/services/auth-service.service';
 import { inject } from '@angular/core';
 
 export const reverseAuthGuard: CanActivateFn = () => {
   const _auth = inject(AuthServiceService);
   const _router = inject(Router);
+  const _state = inject(RouterStateSnapshot)
+  const current_url = _state.url;
+
+  if (current_url.includes('access_token') || current_url.includes('otp_expired')) {
+    return true; 
+  }
 
   if (_auth.isLoggedIn()) {
     _router.navigate(['/project']);
