6fba155520e3e0ca9088de04b999bd7bf398e905
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-04 21:52:23 +0300

    feat(auth): edit reset password requirements

--- src/app/features/auth/components/reset-password/reset-password.component.ts
+++ src/app/features/auth/components/reset-password/reset-password.component.ts
@@ -1,19 +1,19 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, OnDestroy, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthServiceService } from '../../services/auth-service.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ToastNotificationService } from '../../../../shared/services/toast-notification.service';
 import { ResetPasswordSchema } from '../../reset-password.schema';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
-export class ResetPasswordComponent {
-private fb = inject(FormBuilder);
+export class ResetPasswordComponent implements OnDestroy{
+  private fb = inject(FormBuilder);
   private _authServie = inject(AuthServiceService);
   private _router = inject(Router);
   _globalMsg = inject(ToastNotificationService)
@@ -23,7 +23,7 @@ private fb = inject(FormBuilder);
   formValue = signal({});
   resetPasswordPlayload!: {password: string};
   passwordValue = signal('');
-
+  private timeOutId : null | number = null
   private state;
 
 
@@ -45,12 +45,13 @@ private fb = inject(FormBuilder);
   });
 
   passwordRequirements = computed(() => {
-    const hasMinLength = this.passwordValue().length >= 8;
-    const hasUpperLowerDigit =
-      /[A-Z]/.test(this.passwordValue()) && /[a-z]/.test(this.passwordValue()) && /\d/.test(this.passwordValue());
+    const hasMinLength = this.passwordValue().length >= 8 && this.passwordValue().length <= 64;
+    const hasUpper = /[A-Z]/.test(this.passwordValue());
+    const hasLower = /[a-z]/.test(this.passwordValue());
+    const hasDigit = /\d/.test(this.passwordValue());
     const hasSpecialChar = /[!@#$%^&*]/.test(this.passwordValue());
 
-    return { hasMinLength, hasUpperLowerDigit, hasSpecialChar };
+    return { hasMinLength, hasUpper,hasLower,hasDigit, hasSpecialChar };
   });
 
   isFormInvalid = computed(() => {
@@ -77,8 +78,16 @@ private fb = inject(FormBuilder);
     this.state = currentNavigation?.extras.state as { accessToken: string };
 
     if (!this.state || !this.state.accessToken) {
+      this._globalMsg.showMsg("Invalid or expired reset link.")
       this._router.navigate(['/login']);
       return
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    if(this.timeOutId){
+      clearTimeout(this.timeOutId)
     }
   }
 
@@ -95,17 +104,22 @@ private fb = inject(FormBuilder);
       };
 
       this._authServie.resetPassword(this.resetPasswordPlayload,this.state.accessToken).subscribe({
-        next: (res) => {
-          console.log(res);
+        next: () => {
+          // console.log(res);
 
           this._authServie.isLoggedIn.set(true);
           this.isSubmitted.set(false);
           this._globalMsg.showMsg('Your password has been updated successfully. You can now log in', 'success');
-          this._router.navigate(['/login']);
+          
+          this.timeOutId = window.setTimeout(()=>{
+            this._router.navigate(['/login']);
+            this.timeOutId = null
+          },3000)
+          
         },
         error: (err) => {
           this.isSubmitted.set(false);
-          console.log(err);
+          // console.log(err);
           const fallbackMsg = 'Reset password failed. Please try again.';
           if(err.error.error_code === "same_password" || err.error.code === 422){
             this._globalMsg.showMsg( "New password should be different from the old password.");
