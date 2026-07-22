bb668c7739212a5442223f3a4ba41afdabb9e6b6
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-22 20:23:47 +0300

    chore(epics): remove unused commented search signals and draft logic

--- src/app/shared/services/current-project-epics.service.ts
+++ src/app/shared/services/current-project-epics.service.ts
@@ -18,21 +18,10 @@ export class CurrentProjectEpicsService {
   isError = signal<boolean>(false);
   total = signal(0);
 
-  // isSearchResEmpty = signal<boolean>(false);
-  // isSearchResError = signal<boolean>(false);
-  // isSearchLoading = signal<boolean>(false);
-
   getCurrentProjectEpics(offset? : number , limit? : number,isAppend?:boolean,searchTerm?:string){
-    // console.log(...arguments)
-    // if(searchTerm !== ''){
-    //   this.isSearchLoading.set(true)
-    // }
      const projectId=this._project_management.selectedProject()?.id
         this._epics_management.getAllEpics(projectId!,offset,limit,searchTerm).subscribe({
           next: (res: HttpResponse<Epic[]>) => {
-            // if(searchTerm !== ''){
-            //   this.isSearchLoading.set(true)
-            // }
     
             this.isloading.set(false);
             if (isAppend) {
@@ -42,14 +31,9 @@ export class CurrentProjectEpicsService {
               this.epics.set(res.body || []);
               
             }
-            
-            // console.log(res.body)
     
             if (this.epics().length == 0) {
               this.isEmpty.set(true);
-              // if(searchTerm !== ''){
-              //   this.isSearchResEmpty.set(true)
-              //  }
             }
             // content range from header ex: 0-4/5 [(start index - end index) / total num]
             const contentRange = res.headers.get('content-range');
@@ -63,10 +47,6 @@ export class CurrentProjectEpicsService {
             // console.log(err)
             this.isloading.set(false);
             this.isError.set(true);
-            // if(searchTerm !== ''){
-            //   this.isSearchLoading.set(false)
-            //   this.isSearchResError.set(true)
-            // }
           },
         });
   }
