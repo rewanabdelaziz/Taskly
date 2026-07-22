6ab620c8ef767702573dcde3154cfd6f1439bda3
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-22 19:58:45 +0300

    feat(epics): update CurrentProjectEpicsService to accept search term

--- src/app/shared/services/current-project-epics.service.ts
+++ src/app/shared/services/current-project-epics.service.ts
@@ -18,16 +18,26 @@ export class CurrentProjectEpicsService {
   isError = signal<boolean>(false);
   total = signal(0);
 
-  getCurrentProjectEpics(offset? : number , limit? : number,isAppend?:boolean){
+  // isSearchResEmpty = signal<boolean>(false);
+  // isSearchResError = signal<boolean>(false);
+  // isSearchLoading = signal<boolean>(false);
+
+  getCurrentProjectEpics(offset? : number , limit? : number,isAppend?:boolean,searchTerm?:string){
     // console.log(...arguments)
+    // if(searchTerm !== ''){
+    //   this.isSearchLoading.set(true)
+    // }
      const projectId=this._project_management.selectedProject()?.id
-        this._epics_management.getAllEpics(projectId!,offset,limit,).subscribe({
+        this._epics_management.getAllEpics(projectId!,offset,limit,searchTerm).subscribe({
           next: (res: HttpResponse<Epic[]>) => {
+            // if(searchTerm !== ''){
+            //   this.isSearchLoading.set(true)
+            // }
     
             this.isloading.set(false);
             if (isAppend) {
-              const newProj = res.body || [];
-              this.epics.update((prev) => [...prev, ...newProj]);
+              const newEpics = res.body || [];
+              this.epics.update((prev) => [...prev, ...newEpics]);
             } else {
               this.epics.set(res.body || []);
               
@@ -37,6 +47,9 @@ export class CurrentProjectEpicsService {
     
             if (this.epics().length == 0) {
               this.isEmpty.set(true);
+              // if(searchTerm !== ''){
+              //   this.isSearchResEmpty.set(true)
+              //  }
             }
             // content range from header ex: 0-4/5 [(start index - end index) / total num]
             const contentRange = res.headers.get('content-range');
@@ -50,6 +63,10 @@ export class CurrentProjectEpicsService {
             // console.log(err)
             this.isloading.set(false);
             this.isError.set(true);
+            // if(searchTerm !== ''){
+            //   this.isSearchLoading.set(false)
+            //   this.isSearchResError.set(true)
+            // }
           },
         });
   }
