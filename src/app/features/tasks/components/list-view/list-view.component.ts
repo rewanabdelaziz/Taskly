cf59c96b25646e4c2965c0dc0df68dc5e5f26dd0
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-24 22:58:42 +0300

    feats(tasks): implements list view functionality

--- src/app/features/tasks/components/list-view/list-view.component.ts
+++ src/app/features/tasks/components/list-view/list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, HostListener, inject, OnInit, signal } from '@angular/core';
 import { ProjectsManagementsService } from '../../../projects/services/projects-managements.service';
 import { Router } from '@angular/router';
 import { TasksManagementService } from '../../services/tasks-management.service';
@@ -8,11 +8,15 @@ import { IconComponent } from '../../../../shared/components/icon/icon.component
 import { DatePipe, NgClass } from '@angular/common';
 import { NameAvatarIconComponent } from '../../../../shared/components/name-avatar-icon/name-avatar-icon.component';
 import { StatusLabelPipe } from '../../pipes/status-label.pipe';
+import { PaginationService } from '../../../../shared/services/pagination.service';
+import { HttpResponse } from '@angular/common/http';
+import { PaginationComponent } from '../../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-list-view',
   standalone: true,
-  imports: [IconComponent,NgClass,NameAvatarIconComponent,DatePipe,StatusLabelPipe],
+  imports: [IconComponent,NgClass,NameAvatarIconComponent,DatePipe,StatusLabelPipe,PaginationComponent],
+  providers:[PaginationService],
   templateUrl: './list-view.component.html',
   styleUrl: './list-view.component.css'
 })
@@ -20,30 +24,90 @@ export class ListViewComponent implements OnInit{
   private _projects_management = inject(ProjectsManagementsService)
   private _tasks_management = inject(TasksManagementService)
   private _toast = inject(ToastNotificationService)
+   _pagination = inject(PaginationService);
   private _router = inject(Router)
   currentProject = this._projects_management.selectedProject
-  tasks = signal<Task[]| null>(null)
+  tasks = signal<Task[]>([])
   isLoading = signal(false)
   isEmpty = signal(false)
   isError = signal(false)
+  total = signal(0)
+
+
+  currentLength = computed(() => 
+    this._pagination.currentLength(this.tasks().length)
+  )
+  endPageNum = computed(() => 
+    this._pagination.getEndPageNum(this.total())
+  );
+  
+  onPageChange(newPage: number) {
+    this._pagination.goToPage(newPage,this.total());
+    this.getTasks();
+  }
+  
+  isMobileNow = signal<boolean>(false);
 
   ngOnInit(): void {
+    this._pagination.init(4);
    this.getTasks()
+   this.checkScreenSize();
   }
 
+   @HostListener('window:scroll', [])
+    onWindowScroll() {
+      if (!this.isMobileNow() || this.isLoading() || this._pagination.currentPage() >= this.endPageNum()) return;
+  
+      const pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.clientHeight;
+      const max = document.documentElement.scrollHeight;
+  
+      if (pos >= max - 150) {
+        this._pagination.currentPage.update((prev) => prev + 1);
+        this.getTasks(true);
+      }
+    }
+  
+    @HostListener('window:resize', [])
+    checkScreenSize() {
+      const wasMobile = this.isMobileNow();
+      this.isMobileNow.set(window.innerWidth < 1024); // lg:1024px
+  
+      if (wasMobile && !this.isMobileNow()) {
+        this._pagination.currentPage.set(1);
+        this.getTasks(false);
+      }
+    }
 
-  getTasks(){
-      this._tasks_management.getProjectTasksbyStatus(this.currentProject()?.id!).subscribe({
-        next: (res:Task[])=>{
+
+  getTasks(isAppend = false){
+     if (!isAppend) {
+      this.isEmpty.set(false);
+      this.isLoading.set(true);
+    }
+    this.isError.set(false);
+
+      this._tasks_management.getProjectTasksbyStatus(this.currentProject()?.id!,undefined,this._pagination.offset(),this._pagination.limit()).subscribe({
+        next: (res: HttpResponse<Task[]>)=>{
           this.isLoading.set(false)
-          if(res.length === 0){
-            this.isEmpty.set(true)
-          }else{
-            this.isEmpty.set(false)
-          }
-          this.tasks.set(res)
-          
-          console.log(this.tasks())
+           if (isAppend) {
+              const newTask = res.body || [];
+              this.tasks.update((prev) => [...prev, ...newTask]);
+            } else {
+              this.tasks.set(res.body || []);
+              
+            }
+            
+            // console.log(res.body)
+    
+            if (this.tasks().length == 0) {
+              this.isEmpty.set(true);
+            }
+            const contentRange = res.headers.get('content-range');
+            if (contentRange) {
+              const parts = contentRange.split('/');
+              const total = parseInt(parts[1]);
+              this.total.set(total);
+            }
           
         },
         error:(err)=>{
@@ -60,6 +124,8 @@ export class ListViewComponent implements OnInit{
     this._router.navigate(['/project',this.currentProject()?.id,'tasks','new'])
   }
 
-  
+ 
+
+
 
 }
