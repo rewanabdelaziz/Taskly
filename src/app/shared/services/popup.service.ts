cd1f19bb8bde55d23fd61c8b5cfe965598df1ddf
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-30 00:35:23 +0300

    feat(shared): add mobilePosition option and manage body overflow in PopupService

--- src/app/shared/services/popup.service.ts
+++ src/app/shared/services/popup.service.ts
@@ -4,6 +4,7 @@ import { Injectable, signal, Type } from '@angular/core';
 export interface ModalConfig {
   component: Type<any>;
   inputs?: Record<string, any>;
+  mobilePosition?: 'center' | 'bottom-sheet';
 }
 
 @Injectable({
@@ -13,15 +14,15 @@ export class PopupService {
   isOpen = signal<boolean>(false);
   activePopUp = signal<ModalConfig | null>(null);
 
-  open(component: Type<any>, inputs?: Record<string, any>) {
-    this.activePopUp.set({ component, inputs });
+  open(component: Type<any>, inputs?: Record<string, any>,mobilePosition?: 'center' | 'bottom-sheet') {
+    this.activePopUp.set({ component, inputs,mobilePosition });
     this.isOpen.set(true);
-    console.log(this.isOpen())
-    console.log("inside service",...arguments)
+    document.body.classList.add('overflow-hidden');
   }
 
   close() {
     this.isOpen.set(false);
     this.activePopUp.set(null);
+    document.body.classList.remove('overflow-hidden');
   }
 }
