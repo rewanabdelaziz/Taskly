79b5cadaa754f1e526af888c1b76822f8df14500
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-07-31 01:21:18 +0300

    chore(env): add appUrl to environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://vpugcrolnalqsehhpein.supabase.co',
-  supabase_api_key: 'sb_publishable_Unq_YOOIQmkIqI8yOyEZ7A_OGst2f9p'
+  supabase_api_key: 'sb_publishable_Unq_YOOIQmkIqI8yOyEZ7A_OGst2f9p',
+  app_url : 'https://taskly-omega-lyart.vercel.app'
 };
