9823d3e44be997e5c8cfedfcabcbfb84c04c2464
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-08-02 01:14:49 +0300

    feat(statistics): implement statistics service

---
+++ src/app/features/statistics/services/statistics.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { ApiEndpoints } from '../../../core/constants/api-endpoints';
+import { CalendarPayLoad, CalendarResponse } from '../models/statistics';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatisticsService {
+
+   private _http = inject(HttpClient);
+  baseUrl = environment.baseUrl;
+
+
+   getCalendar(startDate: string, endDate: string , projectId: string | null = null , status: string | null = null) : Observable<CalendarResponse> {
+    const params: CalendarPayLoad = {
+      "p_start_date":startDate,
+      "p_end_date":endDate,
+      "p_project_id":projectId,
+      "p_status": status
+    
+    }
+
+    return this._http.post<CalendarResponse>(`${this.baseUrl}${ApiEndpoints.CALENDAR}`,params);
+   }
+  
+
+
+}
